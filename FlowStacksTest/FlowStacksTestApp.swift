3df44f1e62b5eb63e4b5ec4e998110f48eebd536
Ryan Donnelly <ryan.donnelly@detroitlabs.com>
2023-10-27 09:05:38 -0400

    Initial Commit

---
+++ FlowStacksTest/FlowStacksTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowStacksTestApp.swift
+//  FlowStacksTest
+//
+//  Created by Ryan Donnelly on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct FlowStacksTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
